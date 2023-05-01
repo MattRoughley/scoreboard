ba322ffa195182074ecc36c819a07d5bb21c16ca
Matthew Roughley <mattroughley@gmail.com>
2023-05-01 13:10:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let homeScoreboard = document.getElementById("home-score")
+let guestScoreboard = document.getElementById("guest-score")
+
+let= homePoint = 0
+let = guestPoint = 0
+
+function plusOneHome(){
+    homeScoreboard.textContent = homePoint += 1
+
+}
+
+function plusTwoHome(){
+    homeScoreboard.textContent = homePoint += 2
+}
+
+function plusThreeHome(){
+    homeScoreboard.textContent = homePoint += 3
+}
+
+function plusOneGuest(){
+    guestScoreboard.textContent = guestPoint += 1
+
+}
+
+function plusTwoGuest(){
+   guestScoreboard.textContent = guestPoint += 2
+}
+
+function plusThreeGuest(){
+   guestScoreboard.textContent = guestPoint += 3
+}
